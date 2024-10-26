82bbe8ab3d47e7708ebef02c7cc45df6e98ddaef
Alan San <alansandovalt1@gmail.com>
2024-10-26 16:12:28 -0600

    use las collecciones de astro 5 beta

---
+++ src/content/config.ts
@@ -0,0 +1,14 @@
+import { defineCollection, z } from "astro:content";
+
+const wines = defineCollection({
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    price: z.string(),
+    variedad: z.string(),
+    graduation: z.string(),
+    image: z.string(),
+  }),
+});
+
+export const collections = { wines }
