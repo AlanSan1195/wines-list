e2dd91f46bb5ccb4ae352ba869337eb61e71ce46
Alan San <alansandovalt1@gmail.com>
2024-10-30 20:03:41 -0600

    fix: update Buttom styling and integrate active state in WineCard and ButtomMaridage components

--- src/components/ButtomMaridage.jsx
+++ src/components/ButtomMaridage.jsx
@@ -1,19 +1,15 @@
-import { useState } from "react";
+
 import "./Buttom.css";
 
-export default function ButtomMaridage({ vino, price}) {
-  const [enable, setEnable] = useState(true);
-  const classEnable = enable ? "desactive" : "active";
-  const handelClick = ( )=>{
-    console.log(enable)
-    setEnable(!enable)
-  }
+export default function ButtomMaridage({ vino, price, isActive, select, toggleIsActive}) {
 
+  const classEnable = isActive ? "active" : "desactive";
+  
 
   return (
     <div className="flex justify-between items-center mr-4">
 
-      <button onClick={handelClick} className={classEnable} >
+      <button onClick={toggleIsActive} className={classEnable} >
         Maridage
       </button>
       <p className="text-xs font-bold">{price}</p>
--- src/components/WineCard.jsx
+++ src/components/WineCard.jsx
@@ -1,4 +1,5 @@
 import ButtomMaridage from "./ButtomMaridage.jsx";
+import { useState } from "react";
 
 export default function WineCard({
   title,
@@ -7,15 +8,43 @@ export default function WineCard({
   variedad,
   graduation,
   image,
+  index
+  
 }) {
+  const [isActive, setIsActive] = useState(false);
+  const [selected, setSelcted] = useState(false);
+
+  // hacer que cambie si esta activo
+  const toggleIsActive = () => {
+    console.log(isActive);
+    setIsActive(!isActive);
+
+  };
+   const select = () => {
+    setSelcted(!selected);
+   
+  };
+
+
+
   return (
-    <article class=" relative flex-col justify-center items-center border-[1px] border-gray-300/10 rounded-lg  shadow-md bg-black/40     ">
-      <div class="  absolute -top-12    shadow-xl ">
-        <img src={`/${image}`} alt={title} class=" max-w-28 " />
+
+    <article
+      onClick={select}
+      
+      className={`relative flex-col justify-center items-center rounded-lg border-[1px] border-white/30 bg-black transition-transform duration-500    ${
+        selected
+          ? "transform translate-y-0"
+          : "translate-y-10" && !isActive ? " transform translate-y-40" : " translate-y-0"
+      }  `}
+    >
+      <div class="  absolute -top-10 left-4 ">
+        <img src={`/${image}`} alt={title} class=" max-w-24 wine-card-div" />
       </div>
-      <div class=" ml-32 flex flex-col gap-y-1  px-2 my-4 text-pretty      ">
-        <h2 class=" font-bold text-xl mb-2">{title}</h2>
-        <p class=" opacity-80 text-xs mb-2">{description}</p>
+      <div class=" ml-[105px] flex flex-col gap-y-1  px-2 my-3 text-pretty       ">
+        <h2 class=" font-bold text-xl mt-4 mb-2">{title}</h2>
+
+        <p class=" opacity-80 text-sm mb-2">{description}</p>
         <p class=" font-semibold text-xs">
           Composicion: <span class=" font-normal opacity-80">{variedad}</span>
         </p>
@@ -24,16 +53,34 @@ export default function WineCard({
           <span class=" font-normal opacity-80">{graduation}</span>
         </p>
 
-        <ButtomMaridage client:load price={price} vino={title} />
+        <ButtomMaridage
+          client:load
+          price={price}
+          vino={title}
+          isActive={isActive}
+          toggleIsActive={toggleIsActive}
+        />
       </div>
-      <div class=" flex flex-col gap-y-2 px-8 my-10 text-pretty ">
-        <div class=" flex gap-x-2 items-center">
-          <p class=" font-bold">Entradas:</p> <a href="#">Bocaditos</a>
+      <div
+        id="maridage-div"
+        className={`transition-max-height duration-500 ease-in-out overflow-hidden ${
+          isActive ? "max-h-80" : "max-h-0"
+        } flex flex-col gap-y-2 px-8 mt-9 mb-6 text-pretty`}
+      >
+        <div className="flex gap-x-2 items-center">
+          <p className="font-bold">Entradas:</p> <a href="#">Bocaditos</a>
         </div>
-        <div class=" flex gap-x-2 items-center ">
-          <p class=" font-bold">Carne:</p> <a href="#">Bife de lomo</a>
+        <div className="flex gap-x-2 items-center">
+          <p className="font-bold">Carne:</p> <a href="#">Bife de lomo</a>
+        </div>
+        <div>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vero quod
+            autem quos repellat commodi id voluptatem placeat, possimus itaque.
+          </p>
         </div>
       </div>
     </article>
+
   );
 }
